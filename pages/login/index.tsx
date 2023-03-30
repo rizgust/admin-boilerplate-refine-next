b5e4707b67cf7fb86d95987fc556543e3768a744
rizgust <rizgust.dev@gmail.com>
2023-03-30 07:59:16 +0700

    [ADD] initial project refine with next + MUI

---
+++ pages/login/index.tsx
@@ -0,0 +1,45 @@
+import { AuthPage } from "@refinedev/mui";
+
+import { GetServerSideProps } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { authProvider } from "src/authProvider";
+
+export default function Login() {
+  return (
+    <AuthPage
+      type="login"
+      formProps={{
+        defaultValues: {
+          email: "demo@refine.dev",
+          password: "demodemo",
+        },
+      }}
+    />
+  );
+}
+
+Login.noLayout = true;
+
+export const getServerSideProps: GetServerSideProps<{}> = async (context) => {
+  const { authenticated, redirectTo } = await authProvider.check(context);
+
+  const translateProps = await serverSideTranslations(context.locale ?? "en", [
+    "common",
+  ]);
+
+  if (authenticated) {
+    return {
+      props: {},
+      redirect: {
+        destination: redirectTo ?? "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      ...translateProps,
+    },
+  };
+};
