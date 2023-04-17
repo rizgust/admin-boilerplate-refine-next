975cdeb6323ebf19e823deeedf4fb007e599000d
rizgust <rizgust.dev@gmail.com>
2023-04-17 10:50:17 +0700

    [ADD] forgot password page

---
+++ pages/forgot-password/index.tsx
@@ -0,0 +1,40 @@
+import { AuthPage } from "@components/pages/auth";
+import { GetServerSideProps } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { mvAuthProvider } from "src/mvAuthProvider";
+
+export default function ForgotPassword() {
+    return (
+        <AuthPage
+            type="forgotPassword"
+        />
+    );
+}
+
+ForgotPassword.noLayout = true;
+
+export const getServerSideProps: GetServerSideProps<{}> = async (context) => {
+    const { authenticated, redirectTo } = await mvAuthProvider.check(context);
+
+    const translateProps = await serverSideTranslations(
+        context.locale ?? "en",
+        ["common"],
+    );
+
+    if (authenticated) {
+        return {
+            props: {},
+            redirect: {
+                destination: redirectTo ?? "/",
+                permanent: false,
+            },
+        };
+    }
+
+    return {
+        props: {
+            ...translateProps,
+        },
+    };
+};
+
