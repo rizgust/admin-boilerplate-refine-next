b50ab1d27e0fc74c49ee372adad653715d1aaf0c
rizgust <rizgust.dev@gmail.com>
2023-04-17 10:50:02 +0700

    [ADD] login page

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,84 +1,31 @@
-import { useLogin } from "@refinedev/core";
-
-import { Box, Button, Container, Typography } from "@mui/material";
-    import { ThemedTitleV2 } from "@refinedev/mui";
-    
+import { AuthPage } from "@components/pages/auth";
 import { GetServerSideProps } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import { useTranslate } from "@refinedev/core";
-
-import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]";
+import { mvAuthProvider } from "src/mvAuthProvider";
 
 export default function Login() {
-    const { mutate: login } = useLogin();
-
-    const t = useTranslate();
-
-    
-        return (
-            <Container
-            style={{
-                height: "100vh",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-            }}
-            >
-            <Box
-                display="flex"
-                gap="36px"
-                justifyContent="center"
-                flexDirection="column"
-            >
-                <ThemedTitleV2
-                collapsed={false}
-                wrapperStyles={{
-                    fontSize: "22px",
-                    justifyContent: "center",
-                }}
-                />
-
-                <Button style={{ width: "240px" }} variant="contained" size="large" onClick={() => login({})}>
-                    {t("pages.login.signin", "Sign in")}
-                </Button>
-                <Typography align="center" color={"text.secondary"} fontSize="12px">
-                Powered by
-          <img
-            style={{ padding: "0 5px" }}
-            alt="Auth0"
-            src="https://refine.ams3.cdn.digitaloceanspaces.com/superplate-auth-icons%2Fauth0-2.svg"
-          />
-          Auth0
-                </Typography>
-            </Box>
-            </Container>
-        );
-
-
-
+    return (
+        <AuthPage
+            type="login"
+        />
+    )
 }
 
 Login.noLayout = true;
 
 export const getServerSideProps: GetServerSideProps<{}> = async (context) => {
-
-      const session = await getServerSession(
-        context.req,
-        context.res,
-        authOptions,
-    );
+    const { authenticated, redirectTo } = await mvAuthProvider.check(context);
 
     const translateProps = await serverSideTranslations(
         context.locale ?? "en",
         ["common"],
     );
 
-    if (session) {
+    if (authenticated) {
         return {
             props: {},
             redirect: {
-                destination: "/",
+                destination: redirectTo ?? "/",
                 permanent: false,
             },
         };
